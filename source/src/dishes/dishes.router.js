012df0d8ecdb0c29b3d7d9af5df549e0c6b7fad3
evolentDelight <evolentDelight@outlook.com>
2022-11-26 16:20:55 -0800

    Get/Read all dishes

--- source/src/dishes/dishes.router.js
+++ source/src/dishes/dishes.router.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
+const controller = require("./dishes.controller");
+
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
 // TODO: Implement the /dishes routes needed to make the tests pass
+router.get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
